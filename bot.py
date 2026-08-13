f8242398032a4634ea3fde780fc3440b68bb1efa
Anastaredko <stacy31000@mail.ru>
2026-08-13 18:37:20 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -274,7 +274,7 @@ waiting_sprint = storage.get_waiting_sprint()
 if not waiting_sprint:
     storage.create_sprint(SPRINT_START)
 
-bot = telebot.TeleBot("8063432147:AAE82T0GVm3ruwcUNHZ9OWzbwmvp_sc6SeU")
+bot = telebot.TeleBot("8063432147:AAFnfoOakURx1wiPPvJHaar1dq2eSDrqs4E")
 
 # ==================== ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ====================
 def get_status_emoji(status):
